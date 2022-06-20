f587c6ff8f457425e6fda2d65a2276dd88717770
luke9598 <moretti.luca1998@gmail.com>
2022-06-20 11:55:57 +0200

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,26 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>() ;
 
+        Logger logger = Logger.getLogger(Main.class.getName());
+
         list.add("luca");
         list.add("pietro");
         list.add("alessandro");
         list.add("vincenzo");
 
-        for (String s : list) {
-            System.out.println(s);
-        }
+        String curliest = list.toString();
+        logger.info(curliest);
 
         Collections.sort(list);
 
-        for (String s : list) {
-            System.out.println(s);
-        }
+        String change = list.toString();
+        logger.info(change);
     }
 }
