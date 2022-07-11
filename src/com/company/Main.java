29607ea0741122383acb405dae8f920ae59ee300
luke9598 <moretti.luca1998@gmail.com>
2022-07-11 10:42:17 +0200

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,57 +6,30 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-//        List<String> list = new ArrayList<>() ;
-//
-//        Logger logger = Logger.getLogger(Main.class.getName());
-//
-//        list.add("luca");
-//        list.add("pietro");
-//        list.add("alessandro");
-//        list.add("vincenzo");
-//
-//        String curliest = list.toString();
-//        logger.info(curliest);
-//
-//        Collections.sort(list);
-//
-//        String change = list.toString();
-//        logger.info(change);
 
+        Logger logger = Logger.getLogger(Main.class.getName());
 
-//                Scanner sc = new Scanner(System.in);
-//                System.out.println("Enter a string value: ");
-//                String str = sc.nextLine();
-//                char charArray[] = str.toCharArray();
-//                Arrays.sort(charArray);
-//                System.out.println(new String(charArray));
-
-
-
-        // create string array called names
-//        String names[]
-//                = { "AaA" ,"Rahul", "Ajay", "Gourav", "Riya" ,"zZ"};
         Scanner number = new Scanner(System.in);
-        System.out.println("Enter number of words you wanna sort: ");
+        logger.info("Enter number of words you wanna sort: ");
+
         int n = number.nextInt();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string value: ");
+        logger.info("Enter number of words you wanna sort: ");
         String[] names = new String[n];
 
         for (int i = 0; i < n; i++)
         {
-            System.out.println("Please enter number");
+            logger.info("Enter a String: ");
             names[i] = scanner.nextLine();
         }
         String temp;
-
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
 
-                // to compare one string with other strings
+                // compare one string with other strings
                 if (names[i].compareTo(names[j]) < 0) {
-                    // swapping
+                    // reverse alphabetical
                     temp = names[i];
                     names[i] = names[j];
                     names[j] = temp;
@@ -65,11 +38,10 @@ public class Main {
         }
 
         // print output array
-        System.out.println(
-                "The names in alphabetical order are: ");
-        for (int i = 0; i < n; i++) {
-            System.out.println(names[i]);
-        }
+        logger.info( "The names in alphabetical order are: ");
+        List<String> list = Arrays.asList(names);
+        String sortedList = list.toString();
+        logger.info(sortedList);
 
 
     }
